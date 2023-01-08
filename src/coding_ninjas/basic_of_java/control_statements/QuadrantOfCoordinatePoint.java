72d3d809ffcceb4111a12da70936f2af6f408e66
SandeepKushwaha <sandeepkush327@gmail.com>
2023-01-08 19:16:44 +0530

    Coding Ninjas => Basics of Java : another solution added for QuadrantOfCoordinatePoint.java

--- src/coding_ninjas/basic_of_java/control_statements/QuadrantOfCoordinatePoint.java
+++ src/coding_ninjas/basic_of_java/control_statements/QuadrantOfCoordinatePoint.java
@@ -1,10 +1,10 @@
 package coding_ninjas.basic_of_java.control_statements;
 
 /*
-*
-* problem : https://www.codingninjas.com/codestudio/guided-paths/basics-of-java/content/120302/offering/1461356
-*
-* */
+ *
+ * problem : https://www.codingninjas.com/codestudio/guided-paths/basics-of-java/content/120302/offering/1461356
+ *
+ * */
 
 import java.util.Scanner;
 
@@ -27,5 +27,26 @@ public class QuadrantOfCoordinatePoint {
                                         : (a == 0 && b != 0)
                                             ? "y axis"
                                             : "Origin"));
+
+        /* Using If-else-If statements
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x > 0 && y > 0) {
+            System.out.println("1st Quadrant");
+        } else if (x < 0 && y > 0) {
+            System.out.println("2nd Quadrant");
+        } else if (x < 0 && y < 0) {
+            System.out.println("3rd Quadrant");
+        } else if (x > 0 && y < 0) {
+            System.out.println("4th Quadrant");
+        } else if (x > 0 || x < 0 && y == 0) {
+            System.out.println("x axis");
+        } else if (x == 0 && y > 0 || y < 0) {
+            System.out.println("y axis");
+        } else if (x == 0 && y == 0) {
+            System.out.println("Origin");
+        }
+        */
     }
 }
