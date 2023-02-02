357ce6914a3da65be027cac09c17d45b8b8cf443
SandeepKushwaha <sandeepkush327@gmail.com>
2023-02-03 00:30:54 +0530

    Coding Ninjas => Introduction to Java: recursion solved: failing only 1 test case

--- src/coding_ninjas/introduction_to_java/tests/course_test1/FaultyKeyboard.java
+++ src/coding_ninjas/introduction_to_java/tests/course_test1/FaultyKeyboard.java
@@ -6,34 +6,24 @@ public class FaultyKeyboard {
     public static void main(String[] args) {
         // Write your code here
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
+        int t = sc.nextInt();
+        while (t-- > 0) {
             String typed = sc.next();
             String interpreted = sc.next();
-
             System.out.println(faultyKeyword(interpreted, typed));
         }
         sc.close();
     }
 
     static boolean faultyKeyword(String expected, String typed) {
-        int i = 0;
-        int j = 0;
-        int n = typed.length();
-        char temp = expected.charAt(0);
-
-        while (i < n) {
-            if (typed.charAt(i) == expected.charAt(j)) {
-                temp = expected.charAt(j);
-                j++;
+        int i = 0, j = 0;
+        while (i < expected.length() && j < typed.length()) {
+            if (expected.charAt(i) == typed.charAt(j)) {
                 i++;
-            } else if (typed.charAt(i) != expected.charAt(j) && typed.charAt(i) == temp) {
-                i++;
-            } else {
-                return false;
             }
+            j++;
         }
 
-        return j == expected.length();
+        return i == expected.length();
     }
 }
