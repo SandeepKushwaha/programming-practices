84bb93e93cdb87bc4a506dabdb72e4676d0e4a0d
SandeepKushwaha <sandeepkush327@gmail.com>
2023-02-04 23:31:27 +0530

    Coding Ninjas => Introduction to Java: recursion solved: CheckNumberInArray and FirstIndexOfANumberInAnArray

---
+++ src/coding_ninjas/introduction_to_java/recursion/CheckNumberInArray.java
@@ -0,0 +1,38 @@
+package coding_ninjas.introduction_to_java.recursion;
+
+import java.util.Scanner;
+
+public class CheckNumberInArray {
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = s.nextInt();
+        int[] input = new int[n];
+
+        for (int i = 0; i < n; i++)
+            input[i] = s.nextInt();
+
+        int x = s.nextInt();
+        System.out.println(checkNumber(input, x));
+    }
+
+    public static boolean checkNumber(int[] input, int x) {
+        /* Your class should be named Solution
+         * Don't write main().
+         * Don't read input, it is passed as function argument.
+         * Return output and don't print it.
+         * Taking input and printing output is handled automatically.
+         */
+        return checkNumber(input, x, 0);
+    }
+
+    public static boolean checkNumber(int[] input, int x, int index) {
+        if (index == input.length)
+            return false;
+
+        if (input[index] == x)
+            return true;
+
+        return checkNumber(input, x, index + 1);
+    }
+}
---
+++ src/coding_ninjas/introduction_to_java/recursion/FirstIndexOfANumberInAnArray.java
@@ -0,0 +1,45 @@
+package coding_ninjas.introduction_to_java.recursion;
+
+import java.util.Scanner;
+
+public class FirstIndexOfANumberInAnArray {
+
+    static Scanner s = new Scanner(System.in);
+
+    public static int[] takeInput() {
+        int size = s.nextInt();
+        int[] input = new int[size];
+        for (int i = 0; i < size; i++) {
+            input[i] = s.nextInt();
+        }
+        return input;
+    }
+
+    public static void main(String[] args) {
+        int[] input = takeInput();
+        int x = s.nextInt();
+        System.out.println(firstIndex(input, x));
+    }
+
+    public static int firstIndex(int[] input, int x) {
+        /* Your class should be named Solution
+         * Don't write main().
+         * Don't read input, it is passed as function argument.
+         * Return output and don't print it.
+         * Taking input and printing output is handled automatically.
+         */
+        return firstIndex(input, x, 0);
+    }
+
+    public static int firstIndex(int[] input, int x, int startIndex) {
+        if (startIndex == input.length) {
+            return -1;
+        }
+
+        if (input[startIndex] == x) {
+            return startIndex;
+        }
+
+        return firstIndex(input, x, startIndex + 1);
+    }
+}
