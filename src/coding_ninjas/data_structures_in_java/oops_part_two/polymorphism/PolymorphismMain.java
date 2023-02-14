cb7000388391b502537505107a5becac3e9551ff
SandeepKushwaha <sandeepkush327@gmail.com>
2023-02-14 21:38:06 +0530

    Coding Ninjas => Data Structures in Java: OOPs polymorphism added and final_keyword deleted because of duplicate.

---
+++ src/coding_ninjas/data_structures_in_java/oops_part_two/polymorphism/PolymorphismMain.java
@@ -0,0 +1,12 @@
+package coding_ninjas.data_structures_in_java.oops_part_two.polymorphism;
+
+public class PolymorphismMain {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Car("Sky Blue", 120, 2);
+        vehicle.setColor("Black");
+        // vehicle.numberOfDoors = 4;
+        vehicle.print();// method call decided on the runtime
+
+        // Car car = new Vehicle(); // Vehicle is parent that can't be initialized in child that needs type-cast.
+    }
+}
