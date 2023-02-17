ffe0b7d9f5ea427f9c9a1704e424a3c74c4fd255
SandeepKushwaha <sandeepkush327@gmail.com>
2023-02-17 23:13:49 +0530

    Coding Ninjas => Data Structures in Java: OOPs final_keyword(final member or class) added.

---
+++ src/coding_ninjas/data_structures_in_java/oops_part_two/final_keyword/Car.java
@@ -0,0 +1,27 @@
+package coding_ninjas.data_structures_in_java.oops_part_two.final_keyword;
+
+public class Car extends Vehicle {
+    int numberOfDoors;
+
+    /* In this constructor need a param color which is associated to super class or need to delete this constructor
+    public Car(int numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+        System.out.println("Car Object created with variable numberOfDoors: " + this.numberOfDoors);
+    }
+    */
+
+    public Car(String color, int maxSpeed, int numberOfDoors) {
+        super(color, maxSpeed); // Vehicle's Constructor called first then current class object created.
+        this.numberOfDoors = numberOfDoors;
+        System.out.println("Car Object created with variables=> color: " + this.getColor()
+                + ", maxSpeed: " + this.getColor()
+                + ", numberOfDoors: " + this.numberOfDoors);
+    }
+
+    @Override
+    public void print() {
+        String temp = super.toString() + "\b, numberOfDoors = " + numberOfDoors + '}';
+        System.out.println(temp);
+    }
+}
+
---
+++ src/coding_ninjas/data_structures_in_java/oops_part_two/final_keyword/FinalKeywordMain.java
@@ -0,0 +1,8 @@
+package coding_ninjas.data_structures_in_java.oops_part_two.final_keyword;
+
+public class FinalKeywordMain {
+    public static void main(String[] args) {
+        Car car = new Car("Grey", 120, 4);
+        car.print();
+    }
+}
